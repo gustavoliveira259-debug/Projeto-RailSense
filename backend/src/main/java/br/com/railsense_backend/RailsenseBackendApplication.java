925ee81ff4409d69e42d6085c79bede6d0d9ffe2
Edsouzaa <edsonhenriqueabreu@gmail.com>
2026-08-28 20:38:21 -0300

    chore: cria base para a criação do backend do projeto

---
+++ backend/src/main/java/br/com/railsense_backend/RailsenseBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.railsense_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailsenseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailsenseBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/railsense_backend/RailsenseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.railsense_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailsenseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
