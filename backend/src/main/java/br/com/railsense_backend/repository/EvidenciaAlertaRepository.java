2f70556df4e2bcb02ba799395e32c4b14c9ea749
Edsouzaa <edsonhenriqueabreu@gmail.com>
2026-09-04 20:53:23 -0300

    feat: adiciona os repositories no backend

---
+++ backend/src/main/java/br/com/railsense_backend/repository/EvidenciaAlertaRepository.java
@@ -0,0 +1,7 @@
+package br.com.railsense_backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.railsense_backend.models.EvidenciaAlerta;
+
+public interface EvidenciaAlertaRepository extends JpaRepository<EvidenciaAlerta, Long> {}
